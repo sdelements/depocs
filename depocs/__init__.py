99462b7666e70e7c041f614d4246957e7195f3cb
Kelvin Chan <kchan@securitycompass.com>
2024-01-22 09:43:11 -0500

    Drop python 2

--- depocs/__init__.py
+++ depocs/__init__.py
@@ -9,10 +9,25 @@ available within a (dynamic) scope, without having to pass them around as
 function arguments. Scoped helps you do this in a safe and convenient way, and
 provides very informative error messages when you do something wrong.
 """
-from six import with_metaclass
-
 import inspect
 import threading
+
+
+# Copied from six
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself with
+    # the actual metaclass.
+    class metaclass(type):
+
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+
+        @classmethod
+        def __prepare__(cls, name, this_bases):
+            return meta.__prepare__(name, bases)
+    return type.__new__(metaclass, 'temporary_class', (), {})
 
 
 class classproperty(property):
@@ -122,7 +137,7 @@ class Scoped(ScopedClass("ScopedBase", (object,), {})):
         A current scope is expected and there isn't one
         """
 
-    class LifecycleError(with_metaclass(ScopedClass, Error)):
+    class LifecycleError(with_metaclass(ScopedClass, Exception)):
         """
         A scope was opened/closed at the wrong time
         """
